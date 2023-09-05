825a46f46d0439a19c9e80ff9931c42f08424d29
MateusuMelo <mateusosmar@hotmail.com>
2023-09-05 13:49:48 -0300

    Relatorio 04 pronto

---
+++ Relatorio-04/ProductAnalyzer.py
@@ -0,0 +1,45 @@
+from database import Database as Data
+from writeAJson import writeAJson
+
+
+class ProductAnalyzer:
+    def __init__(self):
+        self.dataset = Data(database="mercado", collection="produtos")
+        self.dataset.resetDatabase()
+
+    def vendasPDia(self):
+        data = self.dataset.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$group": {"_id": {"data": "$data_compra"},
+                        "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
+            {"$sort": {"_id.data": 1, "total": -1}},
+        ])
+        return writeAJson(data, "Vendas por dia")
+
+    def maisVendido(self):
+        data = self.dataset.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$group": {"_id": "$produtos.descricao", "total": {"$sum": "$produtos.quantidade"}}},
+            {"$sort": {"total": -1}},
+            {"$limit": 1}
+        ])
+        return writeAJson(data, "Mais vendido")
+
+    def maisGasto(self):
+        data = self.dataset.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$group": {"_id": {"cliente": "$cliente_id", "data": "$data_compra"},
+                        "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
+            {"$sort": {"_id.data": 1, "total": -1}},
+            {"$group": {"_id": "$_id.data", "cliente": {"$first": "$_id.cliente"}, "total": {"$first": "$total"}}},
+            {"$limit": 1}
+        ])
+        return writeAJson(data, "Cliente com mais gasto")
+
+    def produtosVendidos(self):
+        data = self.dataset.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$group": {"_id": "$produtos.descricao", "total": {"$sum": "$produtos.quantidade"}}},
+            {"$sort": {"total": -1}}
+        ])
+        return writeAJson(data, "Produtos vendidos")
